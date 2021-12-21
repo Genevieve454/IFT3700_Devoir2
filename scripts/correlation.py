2870fa9a5df79a356e67920b6c79328847936df8
Genevieve <genevieve.paul-hus@umontreal.ca>
2021-12-20 22:36:33 -0500

    predictions 2.3

--- scripts/correlation.py
+++ scripts/correlation.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import json
-import numpy as np
 
 donnees = pd.read_csv('../data/dataB.csv')
 
 matrice_correlation = donnees.corr()
 
+"""
+Matrice de correlation
+"""
 json_matrice = json.dumps(matrice_correlation.to_numpy().tolist())
 with open("../data/corr.json", "w") as fichier:
     fichier.write(json_matrice)
 
 fichier.close()
 
+
+"""
+Liste de colonnes avec la colonne avec laquelle il y a un max de correlation
+"""
 with open("../data/corr.json") as fichier:
     matrice = json.load(fichier)
 
@@ -35,6 +41,9 @@ with open("../data/max_corr.json", "w") as fichier:
 
 fichier.close()
 
+"""
+Liste descendante des index de colonnes avec correlation absolue moyenne
+"""
 matrice_correlation_abs = matrice_correlation.abs().mean()
 liste_moy_abs_corr = list(matrice_correlation_abs)
 liste_sorted = sorted(range(len(liste_moy_abs_corr)), key=lambda k: liste_moy_abs_corr[k], reverse=True)
